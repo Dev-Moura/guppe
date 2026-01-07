f1c1c83db1d0205febc356052cf8785d8e2bc8fb
Fedora-Notebook-Michael <michael.moura72@hotmail.com>
2026-01-06 21:22:47 -0300

    modulo 6, salvamdoa li

--- modulo_6/loop_for.py
+++ modulo_6/loop_for.py
@@ -24,14 +24,63 @@ Exemplos de iteráveis:
     lista = [1, 3, 5, 7, 9]
 - Range
     numeros = range(1, 10)
-"""
 
 nome = 'Geek University'
 lista = [1, 3, 5, 7, 9]
 numeros = range(1, 10)
 
-# Exemplo de for 1
-
+# Exemplo de for 1 (Iterando em uma string)
 for letra in nome:
     print(letra)
 
+
+
+# Exemplo de for 2 (Iterando sobre uma lista)
+for numero in lista:
+    print(numero)
+
+
+# Exemplo de for 3 (Iterando sobre um range)
+# range(valor_inicial, valor_final)
+# OBS: o range começa do 0
+for numero in range(1, 10):
+    print(numero)
+
+    
+enumerate
+
+for i, v in enumerate(nome):
+    print(nome[i])
+    
+
+
+OBS: Quando não precisamos de um valor, podemos descartá-lo utilizando o underline
+for _, l in enumerate(nome):
+    print(l)
+
+
+    
+for valor in enumerate(nome):
+    print(valor[0])
+   
+    """
+
+
+nome = 'Geek University'
+lista = [1, 3, 5, 7, 9]
+numeros = range(1, 10) # temos que transformar em uma lista
+
+
+
+qtd = int(input("Quantas vezes esse loop deve rodar?"))
+soma = 0
+
+
+for n in range(1, qtd):
+    print(f'Imprimindo{n}')
+
+
+for n in range(1, qtd+1):
+    num = int(input(f'Informe o {n}/{qtd} vqlor: '))
+    soma = soma + num
+print(f'A soma é {soma}')
